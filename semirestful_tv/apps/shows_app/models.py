8a0e5a37940d9819d638331925803860d3b613f2
Brett Jory <bjory7@gmail.com>
2019-04-15 12:49:17 -0700

    updated project according to requirements in Semi-Restful TV Shows w Validation assignment

--- semirestful_tv/apps/shows_app/models.py
+++ semirestful_tv/apps/shows_app/models.py
@@ -1,6 +1,21 @@
+from __future__ import unicode_literals
 from django.db import models
+from datetime import date, datetime
 
 # Create your models here.
+
+class ShowManager(models.Manager):
+    def basic_validator(self, postData):
+        errors = {}
+        if len(postData["title"]) < 2:
+            errors["title"] = "Title must be at least 2 characters."
+        if len(postData["network"]) < 3:
+            errors["network"] = "Network must be at least 3 characters."
+        if len(postData["desc"]) < 10:
+            errors["desc"] = "Description must be at least 10 characters."
+        if len(postData["release_date"]) > 0 and datetime.strptime(postData["release_date"], '%Y-%m-%d') > datetime.today() :
+            errors["release_date"] = "Invalid release date."
+        return errors
 
 class Show(models.Model):
     title = models.CharField(max_length=45)
@@ -9,3 +24,4 @@ class Show(models.Model):
     desc = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects = ShowManager()
--- semirestful_tv/apps/shows_app/views.py
+++ semirestful_tv/apps/shows_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponse, redirect
 from .models import *
 from time import strftime, strptime
+from django.contrib import messages
 
 # Create your views here.
 
@@ -32,11 +33,17 @@ def new_show(request):
 # ADD SHOW TO DB
 ###########################
 def add_new_show(request):
+    errors = Show.objects.basic_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect("/shows/new")
     
-    new_show = Show.objects.create(title=request.POST["title"], network=request.POST["network"], release_date=request.POST["release_date"], desc = request.POST["desc"])
-    new_show_id = new_show.id
-    
-    return redirect(f"/shows/{new_show_id}")
+    else:
+        new_show = Show.objects.create(title=request.POST["title"], network=request.POST["network"], release_date=request.POST["release_date"], desc = request.POST["desc"])
+        new_show_id = new_show.id
+        messages.success(request, "Show successfully added.")
+        return redirect(f"/shows/{new_show_id}")
 
 ###########################
 # DISPLAY SHOW INFO
@@ -57,32 +64,27 @@ def display_show(request, show_id):
 # EDIT SHOW ENTRY
 ###########################
 def edit_show(request, show_id):
-    
     show = Show.objects.get(id=show_id)
     context= {
         "show_html": show
     }
-    
+    messages.success(request, "Show successfully updated.")
     return render(request, "shows_app/editshow.html", context)
 
 ###########################
 # SUBMIT EDIT
 ###########################
 def process_edit(request, show_id):
+    errors = Show.objects.basic_validator(request.POST)
     
-    show = Show.objects.get(id=show_id)
-    
-    if len(request.POST["title"]) > 0:
-        show.title = request.POST["title"]
-    if len(request.POST["network"]) > 0:
-        show.network = request.POST["network"]
-    if len(request.POST["release_date"]) > 0:
-        show.release_date = request.POST["release_date"]
-    if len(request.POST["desc"]) > 0:
-        show.desc = request.POST["desc"]
-    show.save()
-    
-    return redirect(f"/shows/{show_id}")
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect(f"/shows/{show_id}/edit")
+    else:
+        show = Show.objects.get(id=show_id)
+        show.save()
+        return redirect(f"/shows/{show_id}")
 
 ###########################
 # REMOVE SHOW FROM DB
